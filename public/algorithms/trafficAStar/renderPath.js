eb8557c8275cd40d8355d6e6216de24b6d2f6196
blochinchi <jashith.blazingdragon@gmail.com>
2022-03-04 23:05:47 +0530

    added rendering support for diagonal paths

--- public/algorithms/trafficAStar/renderPath.js
+++ public/algorithms/trafficAStar/renderPath.js
@@ -7,7 +7,6 @@ function renderPath(){
         })
     })
     finalPath = tempArray
-    console.log(finalPath);
     for(var i = 0; i < finalPath.length; i++){
         if(finalPath[i] !== end && finalPath[i] !== start && !checkpoints.includes(finalPath[i])){
             finalPath[i].show(color(0, 255, 0));
@@ -38,42 +37,10 @@ function renderLine(){
         finalPath.unshift(start);
     }
     noFill();
-    stroke(255, 0, 0);
+    stroke(255);
     beginShape();
-    console.log(finalPath);
 
-    for(var i = 0; i < finalPath.length-1; i++){
-        if(finalPath[i+1].i !== finalPath[i].i && finalPath[i+1].j !== finalPath[i].j){
-            if(finalPath[i+1].i === finalPath[i].i+1){
-                if(finalPath[i+1].j === finalPath[i].j+1){
-                    line((finalPath[i].x)+w/2, (finalPath[i].y)+h/2, (finalPath[i].nextX) , (finalPath[i].nextY));
-                } else if(finalPath[i+1].j === finalPath[i].j-1){
-                    line((finalPath[i].x)+w, (finalPath[i].y), (finalPath[i].nextX)-w/2 , (finalPath[i].nextY)-h/2);
-                }
-            } else if(finalPath[i+1].i === finalPath[i].i-1){
-                if(finalPath[i+1].j === finalPath[i].j+1){
-                    line((finalPath[i].x)+w/2, (finalPath[i].y)+h/2, (finalPath[i].nextX)-w , (finalPath[i].nextY));
-                } else if(finalPath[i+1].j === finalPath[i].j-1){
-                    line((finalPath[i].x), (finalPath[i].y), (finalPath[i].nextX)-w/2 , (finalPath[i].nextY)-h/2);
-                }
-            }
-        } else{
-            if(finalPath[i+1].i !== finalPath[i].i){
-                if(finalPath[i+1].i === finalPath[i].i+1){
-                    line((finalPath[i].x)+w/2, (finalPath[i].y)+h/2, finalPath[i].nextX , (finalPath[i].nextY)-h/2);
-                } else if(finalPath[i+1].i === finalPath[i].i-1){
-                    line((finalPath[i].x), (finalPath[i].y)+h/2, (finalPath[i].nextX)-w/2 , (finalPath[i].nextY)-h/2);
-                }
-            }
-            else if(finalPath[i+1].j !== finalPath[i].j){
-                if(finalPath[i+1].j === finalPath[i].j+1){
-                    line((finalPath[i].x)+w/2, (finalPath[i].y)+h/2, (finalPath[i].nextX)-w/2 , (finalPath[i].nextY));
-                } else if(finalPath[i+1].j === finalPath[i].j-1){
-                    line((finalPath[i].x)+w/2, (finalPath[i].y), (finalPath[i].nextX)-w/2 , (finalPath[i].nextY)-h/2);
-                }
-            }
-        }
-        
+    for(var i = 0; i <= finalPath.length-1; i++){
         if(i !== 0){
             if(finalPath[i-1].i !== finalPath[i].i && finalPath[i-1].j !== finalPath[i].j){
                 if(finalPath[i-1].i === finalPath[i].i+1){
@@ -101,6 +68,38 @@ function renderLine(){
                     if(finalPath[i-1].j === finalPath[i].j+1){
                         line((finalPath[i].x)+w/2, (finalPath[i].y)+h/2, (finalPath[i].nextX)-w/2 , (finalPath[i].nextY));
                     } else if(finalPath[i-1].j === finalPath[i].j-1){
+                        line((finalPath[i].x)+w/2, (finalPath[i].y), (finalPath[i].nextX)-w/2 , (finalPath[i].nextY)-h/2);
+                    }
+                }
+            }
+        }
+        if(i !== finalPath.length-1){
+            if(finalPath[i+1].i !== finalPath[i].i && finalPath[i+1].j !== finalPath[i].j){
+                if(finalPath[i+1].i === finalPath[i].i+1){
+                    if(finalPath[i+1].j === finalPath[i].j+1){
+                        line((finalPath[i].x)+w/2, (finalPath[i].y)+h/2, (finalPath[i].nextX) , (finalPath[i].nextY));
+                    } else if(finalPath[i+1].j === finalPath[i].j-1){
+                        line((finalPath[i].x)+w, (finalPath[i].y), (finalPath[i].nextX)-w/2 , (finalPath[i].nextY)-h/2);
+                    }
+                } else if(finalPath[i+1].i === finalPath[i].i-1){
+                    if(finalPath[i+1].j === finalPath[i].j+1){
+                        line((finalPath[i].x)+w/2, (finalPath[i].y)+h/2, (finalPath[i].nextX)-w , (finalPath[i].nextY));
+                    } else if(finalPath[i+1].j === finalPath[i].j-1){
+                        line((finalPath[i].x), (finalPath[i].y), (finalPath[i].nextX)-w/2 , (finalPath[i].nextY)-h/2);
+                    }
+                }
+            } else{
+                if(finalPath[i+1].i !== finalPath[i].i){
+                    if(finalPath[i+1].i === finalPath[i].i+1){
+                        line((finalPath[i].x)+w/2, (finalPath[i].y)+h/2, finalPath[i].nextX , (finalPath[i].nextY)-h/2);
+                    } else if(finalPath[i+1].i === finalPath[i].i-1){
+                        line((finalPath[i].x), (finalPath[i].y)+h/2, (finalPath[i].nextX)-w/2 , (finalPath[i].nextY)-h/2);
+                    }
+                }
+                else if(finalPath[i+1].j !== finalPath[i].j){
+                    if(finalPath[i+1].j === finalPath[i].j+1){
+                        line((finalPath[i].x)+w/2, (finalPath[i].y)+h/2, (finalPath[i].nextX)-w/2 , (finalPath[i].nextY));
+                    } else if(finalPath[i+1].j === finalPath[i].j-1){
                         line((finalPath[i].x)+w/2, (finalPath[i].y), (finalPath[i].nextX)-w/2 , (finalPath[i].nextY)-h/2);
                     }
                 }
