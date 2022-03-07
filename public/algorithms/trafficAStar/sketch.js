a37dab0abb39566444e6dc66023d69d7c7e9abcb
blochinchi <jashith.blazingdragon@gmail.com>
2022-03-07 22:10:32 +0530

    improved visuals

--- public/algorithms/trafficAStar/sketch.js
+++ public/algorithms/trafficAStar/sketch.js
@@ -15,141 +15,147 @@ let initialized = false;
 let currentEnd;
 let currentStart;
 let current;
+let done = false;
+let counter = 0;
 
 function Spot(i, j) {
-    this.i = i;
-    this.j = j;
-    this.f = 0;
-    this.g = 0;
-    this.h = 0;
-    this.nextI = i+1;
-    this.nextJ = j+1;
-    this.neighbours = [];
-    this.previous = undefined;
-    this.wall = false;
-    this.show = function (color) {
-        fill(color);
-        if (this.wall){
-            if(!checkpoints.includes(this)){
-            fill(0);
-            }
-        }
-        noStroke();
-        rect(this.i * w, this.j * h, w - 1, h - 1);
-    };
-    this.nextX = w*this.nextI;
-    this.nextY = h*this.nextJ;
-    this.x = w*this.i;
-    this.y = h*this.j;
+	this.i = i;
+	this.j = j;
+	this.f = 0;
+	this.g = 0;
+	this.h = 0;
+	this.nextI = i + 1;
+	this.nextJ = j + 1;
+	this.neighbours = [];
+	this.previous = undefined;
+	this.wall = false;
+	this.show = function (color) {
+		fill(color);
+		if (this.wall) {
+			if (!checkpoints.includes(this)) {
+				fill(0);
+			}
+		}
+		noStroke();
+		rect(this.i * w, this.j * h, w - 1, h - 1);
+	};
+	this.nextX = w * this.nextI;
+	this.nextY = h * this.nextJ;
+	this.x = w * this.i;
+	this.y = h * this.j;
 
-    this.mousePressed = function(){
-        if(this.x < mouseX && this.y < mouseY && this.nextX > mouseX && this.nextY > mouseY){
-            let tempCurrentButton;
-            if(!currentButton){
-                if(!start){
-                    tempCurrentButton = "start";
-                }
-                else if(!end){
-                    tempCurrentButton = "end";
-                }
-                else{ 
-                    tempCurrentButton = "wall";
-                }
-            }
-            if(currentButton === "start" || tempCurrentButton === "start"){
-                if(this !== end){
-                    if(checkpoints.includes(this)){
-                        removeFromArray(checkpoints, this);
-                    }
-                    openSet = [];
-                    start = this;
-                    this.wall = false;
-                }
-
-            }
-            else if(currentButton === "end" || tempCurrentButton === "end"){
-                if(this !== start){
-                    if(end){
-                        removeFromArray(checkpoints, end);
-                    }
-                    if(checkpoints.includes(this)){
-                        removeFromArray(checkpoints, this);
-                    }
-                    end = this;
-                    this.wall = false;
-                }
-            }
-            else if(currentButton === "wall" || tempCurrentButton === "wall"){
-                if(this !== start && this !== end && !checkpoints.includes(this)){
-                    if(this.wall === true){
-                        this.wall = false;
-                        if(diagonalAllowed){
-                            diagonalWallsFix(i, j, "add");
-                        }
-                    }
-                    else if(this.wall === false){
-                        this.wall = true
-                        if(diagonalAllowed){
-                            diagonalWallsFix(i, j, "remove");
-                        }
-                    }
-                }
-            }
-            else if(currentButton === "checkpoint"){
-                if(this !== start && this !== end && !this.wall){
-                    if(checkpoints.includes(this)){
-                        removeFromArray(checkpoints, this);
-                    }
-                    else{
-                        checkpoints.push(this);
-                    }
-                }
-            }
-            if(firstStart){
-                setTimeout(startAlgo);
-            }
-            renderEssentials();
-        }
-    }
+	this.mousePressed = function () {
+		if (
+			this.x < mouseX &&
+			this.y < mouseY &&
+			this.nextX > mouseX &&
+			this.nextY > mouseY
+		) {
+			let tempCurrentButton;
+			if (!currentButton) {
+				if (!start) {
+					tempCurrentButton = "start";
+				} else if (!end) {
+					tempCurrentButton = "end";
+				} else {
+					tempCurrentButton = "wall";
+				}
+			}
+			if (currentButton === "start" || tempCurrentButton === "start") {
+				if (this !== end) {
+					if (checkpoints.includes(this)) {
+						removeFromArray(checkpoints, this);
+					}
+					openSet = [];
+					start = this;
+					this.wall = false;
+				}
+			} else if (currentButton === "end" || tempCurrentButton === "end") {
+				if (this !== start) {
+					if (end) {
+						removeFromArray(checkpoints, end);
+					}
+					if (checkpoints.includes(this)) {
+						removeFromArray(checkpoints, this);
+					}
+					end = this;
+					this.wall = false;
+				}
+			} else if (
+				currentButton === "wall" ||
+				tempCurrentButton === "wall"
+			) {
+				if (
+					this !== start &&
+					this !== end &&
+					!checkpoints.includes(this)
+				) {
+					if (this.wall === true) {
+						this.wall = false;
+						if (diagonalAllowed) {
+							diagonalWallsFix(i, j, "add");
+						}
+					} else if (this.wall === false) {
+						this.wall = true;
+						if (diagonalAllowed) {
+							diagonalWallsFix(i, j, "remove");
+						}
+					}
+				}
+			} else if (currentButton === "checkpoint") {
+				if (this !== start && this !== end && !this.wall) {
+					if (checkpoints.includes(this)) {
+						removeFromArray(checkpoints, this);
+					} else {
+						checkpoints.push(this);
+					}
+				}
+			}
+			if (firstStart) {
+				setTimeout(startAlgo);
+			}
+			renderEssentials();
+		}
+	};
 }
 
-function setup(){
-    blockColumns = Math.round(windowWidth/100);
-    blockRows = Math.round(windowHeight/100);
-    var canvas = createCanvas(windowWidth, windowHeight*0.8);
-    canvas.position((windowWidth - width) / 2, windowHeight - height);
-    canvas.parent('algoHolder');
-    w = width/blockColumns;
-    h = height/blockRows;
-    for(var i = 0; i < blockColumns; i++){
-        quadGrid[i] = new Array(blockRows);
-    }
+function setup() {
+	blockColumns = Math.round(windowWidth / 100);
+	blockRows = Math.round(windowHeight / 100);
+	var canvas = createCanvas(windowWidth, windowHeight * 0.8);
+	canvas.position((windowWidth - width) / 2, windowHeight - height);
+	canvas.parent("algoHolder");
+	w = width / blockColumns;
+	h = height / blockRows;
+	for (var i = 0; i < blockColumns; i++) {
+		quadGrid[i] = new Array(blockRows);
+	}
 
-    for(var i = 0; i < blockColumns; i++){
-        for(var j = 0; j < blockRows; j++){
-            quadGrid[i][j] = new Spot(i, j);
-        }
-    }
+	for (var i = 0; i < blockColumns; i++) {
+		for (var j = 0; j < blockRows; j++) {
+			quadGrid[i][j] = new Spot(i, j);
+		}
+	}
 
-    addNeighbours();
+	addNeighbours();
 
-    renderEssentials();
+	renderEssentials();
 }
 
-function draw(){
-    line(0, 0, 0, windowHeight);
-    line(0, 0, windowWidth, 0);
-    stroke(0)
-    strokeWeight(2)
-    if(startAlgorithm){
-        aStarAlg();
-    }
+function draw() {
+	line(0, 0, 0, windowHeight);
+	line(0, 0, windowWidth, 0);
+	stroke(0);
+	strokeWeight(2);
+	if (startAlgorithm) {
+		aStarAlg();
+	}
 }
 
-function mousePressed(){
-    for (var i = 0; i < blockColumns; i++) {
-        for (var j = 0; j < blockRows; j++) {
-            quadGrid[i][j].mousePressed();
-        }
-    }
+function mousePressed() {
+	for (var i = 0; i < blockColumns; i++) {
+		for (var j = 0; j < blockRows; j++) {
+			quadGrid[i][j].mousePressed();
+		}
+	}
 }
--- public/algorithms/trafficAStar/trafficAStar.js
+++ public/algorithms/trafficAStar/trafficAStar.js
@@ -1,174 +1,98 @@
-function aStarAlg(){
-    if(startAlgorithm){
-        let done = false;
-        for(var k = 0; k < checkpoints.length; k++){
-            if(!startAlgorithm){
-                break;
-            }
-            done = false;
-            while(done === false){
-                if(!initialized){
-                    currentEnd = checkpoints[k];
-                    console.log("initialized")
-                    openSet = [];
-                    path = [];
-                    closedSet = [];
-                    if(k !== 0){
-                        currentStart =checkpoints[k-1]
-                    }
-                    else{
-                        currentStart = start;
-                    }
-                    openSet.push(currentStart);
-                    initialized = true;
-                    if(checkpoints.length > 1){
-                        for(var i = 0; i < blockColumns; i++){
-                            for(var j = 0; j < blockRows; j++){
-                                quadGrid[i][j].f = 0;
-                                quadGrid[i][j].g = 0
-                                quadGrid[i][j].h = 0
-                                quadGrid[i][j].previous = null;
-                            }
-                        }
-                    }
-                }
+function aStarAlg() {
+	done = false;
+	if (!initialized) {
+		console.log("initialized");
+		openSet = [];
+		path = [];
+		closedSet = [];
+		if (counter === 0) {
+			currentStart = start;
+			currentEnd = checkpoints[counter];
+		} else if (counter < checkpoints.length) {
+			currentStart = checkpoints[counter - 1];
+			currentEnd = checkpoints[counter];
+		} else {
+			return null;
+		}
+		openSet.push(currentStart);
+		initialized = true;
+		if (checkpoints.length > 1) {
+			for (var i = 0; i < blockColumns; i++) {
+				for (var j = 0; j < blockRows; j++) {
+					quadGrid[i][j].f = 0;
+					quadGrid[i][j].g = 0;
+					quadGrid[i][j].h = 0;
+					quadGrid[i][j].previous = null;
+				}
+			}
+		}
+	}
+	if (openSet.length > 0) {
+		var chosen = 0;
+		for (var i = 0; i < openSet.length; i++) {
+			if (openSet[chosen].f > openSet[i].f) {
+				chosen = i;
+			}
+		}
 
-                if(openSet.length > 0){
-                    var chosen = 0;
-                    for(var i = 0; i < openSet.length; i++){
-                        if(openSet[chosen].f > openSet[i].f){
-                            chosen = i;
-                        }
-                    }
+		current = openSet[chosen];
+		removeFromArray(openSet, current);
+		closedSet.push(current);
 
-                    current = openSet[chosen];
-                    removeFromArray(openSet, current);
-                    closedSet.push(current);
-
-                    if (current === currentEnd) {
-                        console.log("done")
-                        initialized = false;
-                        firstStart = true;
-                        done = true;
-                        addPath(current, true);
-                        if(k+1 === checkpoints.length){
-                            startAlgorithm = false;
-                            // renderPath();
-                        }
-                    }
-                    else{
-                        var neighbours = current.neighbours;
-                        for(var i = 0; i < neighbours.length; i++){
-                            var neighbour = neighbours[i];
-                            if(!closedSet.includes(neighbour) && !neighbour.wall){
-                                if(current.i !== neighbour.i && current.j !== neighbour.j){
-                                    var tempG= current.g+(Math.sqrt(2));
-                                }
-                                else{
-                                    var tempG= current.g+1;
-                                }
-                                var newPath = false
-                                if (openSet.includes(neighbour)) {
-                                    if(neighbour.g > tempG){
-                                        neighbour.g = tempG;
-                                        newPath = true;
-                                    }
-                                } else{
-                                    neighbour.g = tempG;
-                                    newPath = true;
-                                    openSet.push(neighbour);
-                                }
-                                if(newPath){
-                                    neighbour.previous = current;
-                                    neighbour.h = heuristic(neighbour, currentEnd);
-                                    neighbour.f = neighbour.g + neighbour.h;
-                                }
-                            }
-                        }
-                        addPath(current, false);
-                    }
-                } 
-                else{
-                    console.log("no solution");
-                    firstStart = true;
-                    startAlgorithm = false;
-                    console.log(path);
-                    path = [];
-                    renderEssentials();
-                    console.log(quadGrid);
-                    done = true;
-                    initialized = false;
-                }
-            }
-        }
-    }
-}
-
-function mainAlg(k){
-    if(openSet.length > 0){
-                    var chosen = 0;
-                    for(var i = 0; i < openSet.length; i++){
-                        if(openSet[chosen].f > openSet[i].f){
-                            chosen = i;
-                        }
-                    }
-
-                    current = openSet[chosen];
-                    removeFromArray(openSet, current);
-                    closedSet.push(current);
-
-                    if (current === currentEnd) {
-                        console.log("done")
-                        initialized = false;
-                        firstStart = true;
-                        done = true;
-                        addPath(current, true);
-                        if(k+1 === checkpoints.length){
-                            startAlgorithm = false;
-                            // renderPath();
-                        }
-                    }
-                    else{
-                        var neighbours = current.neighbours;
-                        for(var i = 0; i < neighbours.length; i++){
-                            var neighbour = neighbours[i];
-                            if(!closedSet.includes(neighbour) && !neighbour.wall){
-                                if(current.i !== neighbour.i && current.j !== neighbour.j){
-                                    var tempG= current.g+(Math.sqrt(2));
-                                }
-                                else{
-                                    var tempG= current.g+1;
-                                }
-                                var newPath = false
-                                if (openSet.includes(neighbour)) {
-                                    if(neighbour.g > tempG){
-                                        neighbour.g = tempG;
-                                        newPath = true;
-                                    }
-                                } else{
-                                    neighbour.g = tempG;
-                                    newPath = true;
-                                    openSet.push(neighbour);
-                                }
-                                if(newPath){
-                                    neighbour.previous = current;
-                                    neighbour.h = heuristic(neighbour, currentEnd);
-                                    neighbour.f = neighbour.g + neighbour.h;
-                                }
-                            }
-                        }
-                        addPath(current, false);
-                    }
-                } 
-                else{
-                    console.log("no solution");
-                    firstStart = true;
-                    startAlgorithm = false;
-                    console.log(path);
-                    path = [];
-                    renderEssentials();
-                    console.log(quadGrid);
-                    done = true;
-                    initialized = false;
-                }
+		if (current === currentEnd) {
+			console.log("done");
+			initialized = false;
+			firstStart = true;
+			done = true;
+			addPath(current, true);
+			counter = counter + 1;
+			if (currentEnd === end) {
+				startAlgorithm = false;
+				counter = 0;
+			}
+		} else {
+			var neighbours = current.neighbours;
+			for (var i = 0; i < neighbours.length; i++) {
+				var neighbour = neighbours[i];
+				if (!closedSet.includes(neighbour) && !neighbour.wall) {
+					if (
+						current.i !== neighbour.i &&
+						current.j !== neighbour.j
+					) {
+						var tempG = current.g + Math.sqrt(2);
+					} else {
+						var tempG = current.g + 1;
+					}
+					var newPath = false;
+					if (openSet.includes(neighbour)) {
+						if (neighbour.g > tempG) {
+							neighbour.g = tempG;
+							newPath = true;
+						}
+					} else {
+						neighbour.g = tempG;
+						newPath = true;
+						openSet.push(neighbour);
+					}
+					if (newPath) {
+						neighbour.previous = current;
+						neighbour.h = heuristic(neighbour, currentEnd);
+						neighbour.f = neighbour.g + neighbour.h;
+					}
+				}
+			}
+			addPath(current, false);
+		}
+	} else {
+		console.log("no solution");
+		firstStart = true;
+		startAlgorithm = false;
+		console.log(path);
+		path = [];
+		renderEssentials();
+		console.log(quadGrid);
+		done = true;
+		initialized = false;
+		counter = 0;
+	}
 }
