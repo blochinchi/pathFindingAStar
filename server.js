d9dc5c110c8b9ee26f8db01389e5f4bf9ea924e2
blochinchi <jashith.blazingdragon@gmail.com>
2022-03-07 23:42:33 +0530

    made changes for heroku

--- server.js
+++ server.js
@@ -3,33 +3,31 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const app = express();
 app.use(express.static("public"));
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function(req, res){
-    res.render("index");
-})
+app.get("/", function (req, res) {
+	res.render("index");
+});
 
-app.post("/", function(req, res){
-    if(req.body.trafficAStar !== undefined){
-        res.redirect("/trafficAStar");
-    }
-    else if(req.body.railRoadAStar !== undefined){
-        res.redirect("/railAStar");
-    }
-    else{
-        res.redirect("/");
-    }
-})
+app.post("/", function (req, res) {
+	if (req.body.trafficAStar !== undefined) {
+		res.redirect("/trafficAStar");
+	} else if (req.body.railRoadAStar !== undefined) {
+		res.redirect("/railAStar");
+	} else {
+		res.redirect("/");
+	}
+});
 
-app.get("/trafficAStar", function(req, res){
-    res.render("trafficAStar");
-})
+app.get("/trafficAStar", function (req, res) {
+	res.render("trafficAStar");
+});
 
-app.get("/railAStar", function(req, res){
-    res.render("railAStar");
-})
+app.get("/railAStar", function (req, res) {
+	res.render("railAStar");
+});
 
-app.listen(3000, function(){
-    console.log("BANZAI");
-})
+app.listen(process.env.PORT, function () {
+	console.log("BANZAI");
+});
