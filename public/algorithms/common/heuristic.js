2d4f429521becc8474b3cc602cec0c08c584355a
blochinchi <jashith.blazingdragon@gmail.com>
2022-03-09 15:32:42 +0530

    new heuristic

--- public/algorithms/common/heuristic.js
+++ public/algorithms/common/heuristic.js
@@ -1,4 +1,9 @@
 function heuristic(a, b) {
-  var d = dist(a.i, a.j, b.i, b.j);
-  return d;
+	var xDistance = Math.abs(a.i - b.i);
+	var yDistance = Math.abs(a.j - b.j);
+	if (!diagonalAllowed) {
+		return xDistance + yDistance;
+	} else {
+		return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+	}
 }
--- public/algorithms/trafficAStar/assessNode.js
+++
@@ -1,9 +0,0 @@
-function assessNode() {
-	for (var i = 0; i < blockColumns; i++) {
-		for (var j = 0; j < blockRows; j++) {
-			quadGrid[i][j].g = heuristic(quadGrid[i][j], currentStart);
-			quadGrid[i][j].h = heuristic(quadGrid[i][j], currentEnd);
-			quadGrid[i][f].f = quadGrid[i][j].h + quadGrid[i][j].g;
-		}
-	}
-}
